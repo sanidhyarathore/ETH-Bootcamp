ea5f9b6203957fa5c75257b73e003c3410abdc53
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2025-05-01 21:25:58 +0530

    fixed 5th test

--- Contract-Puzzles/test/game5Test.js
+++ Contract-Puzzles/test/game5Test.js
@@ -1,31 +1,45 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
-const { ethers } = require('hardhat');
+const { ethers, network } = require("hardhat");
+const { assert } = require("chai");
 
-describe('Game5', function () {
-  async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+describe("Game5", function () {
+  it("should be a winner", async function () {
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game };
-  }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-    // good luck
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+    const thresholdBN = BigInt(threshold.toLowerCase());
 
-    const signers = await ethers.getSigners();
+    let winningWallet;
+    let i = 0;
 
-    for (const signer of signers) {
-      const ans = await signer.getAddress();
-      if (
-        BigInt(ans.toLowerCase()) < BigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf")
-      ) {
-        await game.connect(signer).win();
+    while (true) {
+      const wallet = ethers.Wallet.createRandom();
+      const addrBN = BigInt(wallet.address.toLowerCase());
+
+      if (addrBN < thresholdBN) {
+        winningWallet = wallet;
+        console.log("Found winning wallet:", wallet.address);
         break;
       }
+      if (++i % 1000 === 0) console.log("Tried", i, "wallets...");
     }
 
-    // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    // Fund the wallet with ETH
+    const [funder] = await ethers.getSigners();
+    const tx = await funder.sendTransaction({
+      to: winningWallet.address,
+      value: ethers.utils.parseEther("1"),
+    });
+    await tx.wait();
+
+    // Connect winning wallet to provider
+    const connectedWinningWallet = winningWallet.connect(ethers.provider);
+
+    // Call win()
+    const gameWithWinningWallet = game.connect(connectedWinningWallet);
+    const winTx = await gameWithWinningWallet.win();
+    await winTx.wait();
+
+    assert(await game.isWon(), "You did not win the game");
   });
 });
